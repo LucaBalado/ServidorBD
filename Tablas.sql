f71d8b3a3c3ec98bc6c223563b3599fd20ad2497
Luca Balado <lucabalado@gmail.com>
2023-09-25 17:10:28 -0300

    Completar tablas

---
+++ Tablas.sql
@@ -0,0 +1,8 @@
+INSERT INTO "user" (username, password)
+VALUES ('homelochino', '123' ),
+	('nashe', '111'),
+	('Yo', '912');
+INSERT INTO "post"(author_id, title, body)
+VALUES ('1', 'homelochino', 'soy homelo pelo homelo chino'),
+	('3', 'yyyy', 'ñkpolpl3'),
+	('2', 'kkkk', 'voasdcascd')
